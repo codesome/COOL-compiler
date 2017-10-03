dba441ae968847b414c92f99106782089e7bfb50
Ganesh Vernekar <cs15btech11018@iith.ac.in>
2017-10-03 22:23:13 +0530

    added codegen files

---
+++ codegen/src/java/cool/Codegen.java
@@ -0,0 +1,10 @@
+package cool;
+
+import java.io.PrintWriter;
+
+public class Codegen{
+	public Codegen(AST.program program, PrintWriter out){
+		//Write Code generator code here
+        out.println("; I am a comment in LLVM-IR. Feel free to remove me.");
+	}
+}
